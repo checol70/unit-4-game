ca00f266197730a6b7ab4280e0cb43e7f4f4725e
Sheckerol <checol70@gmail.com>
2018-06-28 19:20:59 -0700

    finished js code, only needs css.

---
+++ assets/javascript/game.js
@@ -0,0 +1,133 @@
+var player;
+var defender;
+var fArray = new Array();
+var waitArea = $("#waitarea");
+var foesRemaining=0;
+
+class Fighter {
+    constructor(attack, hp, name) {
+        this.baseAttackPower = attack;
+        this.attackPower = attack;
+        this.counterPower = attack +10;
+        this.healthPoints = hp;
+        this.name = name;
+        this.hpRevealer = null;
+        this.attackRevealer = null;
+        this.myDiv = null;
+        fArray.push(this);
+        console.log("fighter added: " + this.name)
+    }
+    attack(enemy) {
+        console.log("ATACK!!!")
+        enemy.damaged(this.attackPower)
+        this.attackPower += this.baseAttackPower;
+        if (this.attackRevealer != null)
+            this.attackRevealer.text(this.attackPower);
+    }
+    damaged(int) 
+    {
+        this.healthPoints -= int;
+        if (this.hpRevealer != null)
+        {
+            this.hpRevealer.text(this.healthPoints)
+        }
+        if(player == this && this.healthPoints<= 0)
+        {
+            lose();
+        }
+
+        if (player != this)
+        {
+            if (this.healthPoints > 0) 
+            {
+                player.damaged(this.counterPower);
+
+            }
+            else if (this.healthPoints <= 0) 
+            {
+                defender = null;
+                $("#defenderarea").empty();
+                foesRemaining--;
+                console.log("foesRemaining:",foesRemaining)
+                if(foesRemaining <= 0)
+                {
+                    console.log("wonderful")
+                    win();
+                }
+            }
+        }
+    }
+}
+
+function win()
+{
+    $("#wintext").show();
+}
+
+$("#wintext").hide();
+$("#losetext").hide();
+
+function lose()
+{
+    $("#losetext").show();
+}
+
+addFighters();
+function addFighters() {
+    fighterA = new Fighter(10, 100, "Beat Stick");
+    fighterB = new Fighter(25, 50, "Bow Chick");
+    fighterC = new Fighter(5, 200, "Knight Guy")
+    fighterD = new Fighter(20, 75, "Mage Dude")
+    setFighterIcons();
+    foesRemaining = fArray.length -1;
+
+}
+
+function setFighterIcons() {
+    fArray.forEach(function (e, i) {
+        //bringing in an image of our fighter
+        fighterImage = $("<img>").attr("src", "assets/images/Fighter" + i + ".png");
+        fighterImage.attr("data-fighter-index", i)
+
+        //putting up a div to hold everything
+        fighterDiv = $("<div>").attr("id", e.name).addClass("text-center")
+        fighterDiv.on("click", function () {
+            assignPlayer(e)
+        })
+        e.myDiv = fighterDiv;
+        $("#waitarea").append(fighterDiv);
+        fighterDiv.addClass("col-3");
+        fighterImage.height("150px")
+
+        //this area for showing data
+        fighterDiv.append(fighterImage);
+        fighterDiv.append("<p>" + e.name + "</p>");
+        e.hpRevealer = $("<p>Current Hp <br>" + e.healthPoints + "</p>");
+        e.attackRevealer = $("<p>Current Attack <br>" + e.attackPower + "</p>");
+        fighterDiv.append(e.hpRevealer);
+        fighterDiv.append(e.attackRevealer);
+    })
+}
+
+function assignPlayer(e) {
+    console.log("assigning player")
+    if (!fArray.includes(player)) {
+        player = e;
+        $("#playerarea").append(player.myDiv)
+        console.log(player)
+    }
+    else if (!fArray.includes(defender)) {
+        defender = e;
+        $("#defenderarea").append(defender.myDiv);
+    }
+}
+function attBtnPressed() {
+    console.log("attBtnPressed")
+    if (defender != null) {
+        console.log("player is attacking")
+        player.attack(defender)
+    }
+}
+$("#att-btn").on("click", function (e) {
+    attBtnPressed()
+})
